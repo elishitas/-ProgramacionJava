390381966f4a079281bf2de87a409810a7ac8bb5
Lisa <mariaearaya@mingeneros.gob.ar>
2023-04-17 22:27:51 -0300

    se completa guia 3

--- IntroJava/src/guia3/EjercicioUno.java
+++ IntroJava/src/guia3/EjercicioUno.java
@@ -1,13 +1,25 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/*Crear un programa que dado un número determine si es par o impar.
  */
 package guia3;
+
+import java.util.Scanner;
 
 /**
  *
  * @author araya
  */
 public class EjercicioUno {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese un número");
+        int num = leer.nextInt();
+        if (num % 2 != 0) {
+      //Si el número dividido por 2 su resto NO da 0, entonces:       
+            System.out.println("El número es impar");
+        } else {
+            System.out.println("El número es par");
+        }
+    }
     
 }
